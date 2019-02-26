ba046349c6c18e6852668c199afb8f170fb8f2c5
SystemAddict85 <brandanders85@gmail.com>
2019-02-25 19:15:46 -0600

    text readout

---
+++ vrPianov2/Assets/TextReadout.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextReadout : MonoBehaviour
+{
+    public void PrintToScreen(string text)
+    {
+        GetComponent<TextMeshProUGUI>().text = text;
+    }
+
+}
