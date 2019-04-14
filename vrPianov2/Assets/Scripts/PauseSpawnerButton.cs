78891c0b0e6847f2cf5d7f4327ebd3de44c623b2
k_changgonzalez <k_changgonzalez@tamu.edu>
2019-04-14 16:16:13 -0500

    [Added] Piggy Squish

--- vrPianov2/Assets/Scripts/PauseSpawnerButton.cs
+++ vrPianov2/Assets/Scripts/PauseSpawnerButton.cs
@@ -2,39 +2,34 @@
 
 public class PauseSpawnerButton : MonoBehaviour
 {
-    [SerializeField]
-    private Material stopMat;
-    [SerializeField]
-    private Material playMat;
-
     public float buttonDelay = .5f;
     public bool isReadyToPush = true;
 
     public bool isRunning = true;
 
-    public void OnCollisionEnter(Collision collision)
+    public void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("HandController"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("HandController"))
         {
             if (isReadyToPush)
             {
                 isReadyToPush = false;
                 //StartCoroutine(WaitToPushAgain());
-                var hand = collision.gameObject.GetComponent<PianoHand>();
+                var hand = other.gameObject.GetComponent<PianoHand>();
                 hand.HapticVibration();
+                GetComponent<AudioSource>().Play();
+                GetComponent<Animator>().SetTrigger("isSquish");
                 if (isRunning)
                 {
                     Debug.Log("pause");
                     GameManager.Instance.StopSpawning();
                     isRunning = false;
-                    GetComponent<Renderer>().material = playMat;
                 }
                 else
                 {
                     Debug.Log("start spawning");
                     GameManager.Instance.StartSpawning();
                     isRunning = true;
-                    GetComponent<Renderer>().material = stopMat;
                 }
                 isReadyToPush = true;
             }
